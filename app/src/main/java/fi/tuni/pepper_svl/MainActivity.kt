40d71f4d46d12baa2047f739ac7f9a58970ec4a0
U-DESKTOP-FH1PMIH\Leksi <aleksi.hyttinen@tuni.fi>
2023-01-25 11:21:31 +0200

    Init project

---
+++ app/src/main/java/fi/tuni/pepper_svl/MainActivity.kt
@@ -0,0 +1,37 @@
+package fi.tuni.pepper_svl
+
+import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.`object`.conversation.Say
+import com.aldebaran.qi.sdk.builder.SayBuilder
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
+
+class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        QiSDK.register(this, this)
+    }
+
+    override fun onDestroy() {
+        QiSDK.unregister(this, this)
+        super.onDestroy()
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext?) {
+        val say: Say = SayBuilder.with(qiContext)
+            .withText("Hei ihminen!")
+            .build()
+        say.run()
+    }
+
+    override fun onRobotFocusLost() {
+
+    }
+
+    override fun onRobotFocusRefused(reason: String?) {
+
+    }
+}
