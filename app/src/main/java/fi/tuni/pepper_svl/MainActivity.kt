6e0f2124ddbecf2713b379ead281036dd69e9d2f
aleksihyttinen <alehyt@gmail.com>
2023-01-26 12:45:32 +0200

    Add needed activities

--- app/src/main/java/fi/tuni/pepper_svl/MainActivity.kt
+++ app/src/main/java/fi/tuni/pepper_svl/MainActivity.kt
@@ -1,6 +1,9 @@
 package fi.tuni.pepper_svl
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
@@ -13,6 +16,16 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         QiSDK.register(this, this)
+        val sananlaskuButton : Button = findViewById(R.id.sananlasku_button)
+        sananlaskuButton.setOnClickListener {
+            val intent = Intent(this, SananlaskuActivity::class.java)
+            startActivity(intent)
+        }
+        val labraButton : Button = findViewById(R.id.labra_button)
+        labraButton.setOnClickListener {
+            val intent = Intent(this, LabraActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroy() {
