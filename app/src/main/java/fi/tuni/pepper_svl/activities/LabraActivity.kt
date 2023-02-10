3cb8190ea957c840cbec64f35e2b12390eb130cd
U-DESKTOP-FH1PMIH\Leksi <aleksi.hyttinen@tuni.fi>
2023-02-10 11:56:45 +0200

    Movement changes and start of mapping

--- app/src/main/java/fi/tuni/pepper_svl/activities/LabraActivity.kt
+++ app/src/main/java/fi/tuni/pepper_svl/activities/LabraActivity.kt
@@ -3,7 +3,6 @@ package fi.tuni.pepper_svl.activities
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,22 +12,30 @@ import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
 import com.aldebaran.qi.sdk.builder.*
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
+import com.aldebaran.qi.sdk.`object`.actuation.*
 import com.aldebaran.qi.sdk.`object`.conversation.Chat
 import com.aldebaran.qi.sdk.`object`.conversation.QiChatbot
+import com.aldebaran.qi.sdk.`object`.conversation.Say
 import com.aldebaran.qi.sdk.`object`.conversation.Topic
+import com.aldebaran.qi.sdk.`object`.geometry.Transform
 import com.aldebaran.qi.sdk.`object`.human.Human
 import com.aldebaran.qi.sdk.`object`.humanawareness.HumanAwareness
 import com.aldebaran.qi.sdk.`object`.locale.Language
 import com.aldebaran.qi.sdk.`object`.locale.Locale
 import com.aldebaran.qi.sdk.`object`.locale.Region
-import com.aldebaran.qi.sdk.design.activity.RobotActivity
-import com.aldebaran.qi.sdk.`object`.actuation.*
-import com.aldebaran.qi.sdk.`object`.conversation.Say
-import com.aldebaran.qi.sdk.`object`.image.EncodedImage
+import com.aldebaran.qi.sdk.util.FutureUtils
 import com.softbankrobotics.dx.followme.FollowHuman
-import fi.tuni.pepper_svl.adapters.CustomAdapter
-import fi.tuni.pepper_svl.models.ItemsViewModel
 import fi.tuni.pepper_svl.R
+import fi.tuni.pepper_svl.adapters.CustomAdapter
+import fi.tuni.pepper_svl.data.RobotHelper
+import fi.tuni.pepper_svl.data.SaveFileHelper
+import fi.tuni.pepper_svl.data.Vector2theta
+import fi.tuni.pepper_svl.models.ItemsViewModel
+import java.io.File
+import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
+
 
 class LabraActivity : RobotActivity(), RobotLifecycleCallbacks {
     private lateinit var chat: Chat
@@ -40,6 +47,12 @@ class LabraActivity : RobotActivity(), RobotLifecycleCallbacks {
     private var localizeAndMap: LocalizeAndMap? = null
     private var localize: Localize? = null
     private var explorationMap: ExplorationMap? = null
+    private val filesDirectoryPath = "/sdcard/Maps"
+    private val locationsFileName = "points.json"
+    var savedLocations: TreeMap<String, AttachedFrame> = TreeMap()
+    var robotHelper: RobotHelper? = null
+    var saveFileHelper: SaveFileHelper? = null
+    private val loadLocationSuccess: AtomicBoolean = AtomicBoolean(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_labra)
@@ -66,6 +79,13 @@ class LabraActivity : RobotActivity(), RobotLifecycleCallbacks {
             updateUi(humanAwareness?.engagedHuman != null)
             stopFollowing()
         }
+        startChat()
+        val file = File("/sdcard/Maps", "points.json")
+        Log.i("test", file.toString())
+        if (file.exists()) {
+            loadLocations()
+        }
+        /*
         val say: Say = SayBuilder.with(qiContext)
             .withText("Odota hetki kun tutustun ympäristööni")
             .build()
@@ -119,8 +139,69 @@ class LabraActivity : RobotActivity(), RobotLifecycleCallbacks {
                     }
                 }
             }
-        }
+        }*/
 
+    }
+    fun loadLocations(): Future<Boolean>? {
+        Log.i("test", "load started")
+        return FutureUtils.futureOf<Boolean> { f: Future<Void?>? ->
+            // Read file into a temporary hashmap.
+            val file: File = File(filesDirectoryPath, locationsFileName)
+            if (file.exists()) {
+                val vectors: Map<String, Vector2theta> =
+                    saveFileHelper!!.getLocationsFromFile(filesDirectoryPath, locationsFileName)
+
+                // Clear current savedLocations.
+                savedLocations = TreeMap()
+                val mapFrame: Frame = robotHelper!!.mapFrame
+
+                // Build frames from the vectors.
+                for ((key, value) in vectors) {
+                    // Create a transform from the vector2theta.
+                    val t: Transform = value.createTransform()
+                    Log.d("test", "loadLocations: $key")
+
+                    // Create an AttachedFrame representing the current robot frame relatively to the MapFrame.
+                    val attachedFrame = mapFrame.async().makeAttachedFrame(t).value
+
+                    // Store the FreeFrame.
+                    savedLocations[key] = attachedFrame
+                    loadLocationSuccess.set(true)
+                }
+                Log.d("test", "loadLocations: Done")
+                if (loadLocationSuccess.get()) return@futureOf Future.of<Boolean>(
+                    true
+                ) else throw Exception("Empty file")
+            } else {
+                throw Exception("No file")
+            }
+        }
+    }
+    fun goToLocation(location: String, orientationPolicy: OrientationPolicy) {
+        Log.i("test", location)
+        Log.i("test", robotHelper.toString())
+        Log.i("test", savedLocations.toString())
+        robotHelper!!.goToHelper.checkAndCancelCurrentGoto().thenConsume { aVoid ->
+            robotHelper!!.holdAbilities(true)
+            if (location.lowercase() == "mapframe") {
+                robotHelper!!.goToHelper.goToMapFrame(false, false, orientationPolicy)
+            } else if (location.lowercase() == "ChargingStation") {
+                robotHelper!!.goToHelper.goToChargingStation(
+                    savedLocations[location],
+                    false,
+                    false,
+                    orientationPolicy
+                )
+            } else {
+                // Get the FreeFrame from the saved locations.
+                robotHelper!!.goToHelper.goTo(
+                    savedLocations[location],
+                    false,
+                    false,
+                    orientationPolicy
+                )
+            }
+        }
     }
 
     override fun onRobotFocusLost() {
@@ -189,6 +270,9 @@ class LabraActivity : RobotActivity(), RobotLifecycleCallbacks {
             }
             if(heardPhrase.text == "pysähdy") {
                 stopFollowing()
+            }
+            if(heardPhrase.text == "sohvat") {
+                goToLocation("sohvat", OrientationPolicy.ALIGN_X)
             }
         }
         qiChatbot.addOnEndedListener { endPhrase: String ->
